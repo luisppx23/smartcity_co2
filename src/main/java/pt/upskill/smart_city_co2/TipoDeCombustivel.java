e287393a36d9f20f4efc9732f6a6c3cc95377c17
Pedro Godinho <pedro.a.f.godinho@gmail.com>
2026-03-30 14:21:51 +0100

    SCCO2-98 - correção fator emissao.

--- src/main/java/pt/upskill/smart_city_co2/TipoDeCombustivel.java
+++ src/main/java/pt/upskill/smart_city_co2/TipoDeCombustivel.java
@@ -2,10 +2,10 @@ package pt.upskill.smart_city_co2;
 
 public enum TipoDeCombustivel {
     ELETRICO(0.0, 0.0),
-    HIBRIDO(90.0, 0.010),
-    GASOLINA(150.0, 0.015),
-    DIESEL(130.0, 0.008),
-    GPL(115.0, 0.020);
+    HIBRIDO(120.0, 0.010),
+    GASOLINA(192.0, 0.015),
+    DIESEL(171.0, 0.02),
+    GPL(145.0, 0.025);
 
     private final double fatorEmissaogPorLitro;
     private final double fatorDegradacaoAnual;
